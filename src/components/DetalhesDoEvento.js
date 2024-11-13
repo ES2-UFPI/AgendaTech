ff33b949e6407bf7935a4c62b115d31fa656c354
VivalJose <vivaljoe@ufpi.edu.br>
2024-11-13 20:14:46 -0300

    ultimo update esqueleto 13 novembro

--- src/components/DetalhesDoEvento.js
+++ src/components/DetalhesDoEvento.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const DetalhesDoEvento = ({ evento }) => {
     return (
-        <div>
+        <div className="detalhes-do-evento">
             <h2>{evento.titulo}</h2>
             <p>Data: {evento.data}</p>
-            <p>Local: {evento.local || event.onlineLink}</p>
+            <p>Local: {evento.local}</p>
+            {evento.link && <p>Link: {evento.link}</p>}
+            <p>Categoria: {evento.categoria}</p>
             <p>{evento.descricao}</p>
         </div>
     );
